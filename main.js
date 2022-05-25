249a315ee054dbb1f5606cd24b7af5f7db6e41b0
Vinoshanpradeeps <89023571+Vinoshanpradeeps@users.noreply.github.com>
2022-05-25 18:01:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(400, 350);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('clown_image.png');
+}
