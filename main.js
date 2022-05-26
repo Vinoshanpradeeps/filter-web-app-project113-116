4a39f34780e0ee0ee6d4613a91525d8ece1a3250
Vinoshanpradeeps <89023571+Vinoshanpradeeps@users.noreply.github.com>
2022-05-26 13:27:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
 function preload(){
 
 }
+
 function setup(){
-    canvas = createCanvas(400, 350);
+    canvas = createCanvas(1200, 390);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+  
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function draw(){
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  
+  function gotPoses(results)
+  {
+    if(results.length > 0)
+    {
+      console.log(results);
+      console.log("nose x = " + results[0].pose.nose.x);
+      console.log("nose y = " + results[0].pose.nose.y);
+    }
+  }
 
+function draw(){
+    image(video, 470, 50, 300, 300);
 }
 
 function take_snapshot(){
